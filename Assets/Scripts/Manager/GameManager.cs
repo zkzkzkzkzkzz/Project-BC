a24e2365b36cba81e74144496cc143ecc487ac84
zkzkzkzkzkzz <sto134679@gmail.com>
2025-03-14 11:19:18 +0900

    Feature / 게임매니저 생성 및 싱글톤 적용

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
