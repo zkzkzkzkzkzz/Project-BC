38c2be7162aa9a8f31eb5b217a01aabf03c6e662
zkzkzkzkzkzz <sto134679@gmail.com>
2025-04-09 11:14:51 +0900

    FEAT / 유닛을 타일 기준으로 배치
    - 타일을 발견하지 못했다면 유닛은 기존 위치로 되돌아감

--- Assets/Scripts/Unit/DraggableUnit.cs
+++ Assets/Scripts/Unit/DraggableUnit.cs
@@ -9,9 +9,12 @@ public class DraggableUnit : MonoBehaviour
     private Camera mainCam;
     private bool isDragging = false;
     private Plane dragPlane;
-    private float originY;
+    private Vector3 originPos;
 
-    [SerializeField] float offsetY = 0.5f;
+    [SerializeField] private float offsetY = 0.5f;      // 드래그 시 유닛이 들어올려지는 정도
+    [SerializeField] private LayerMask tileLayerMask;   // 타일만 감지하도록 레이어 설정
+
+    private GameObject hoveredTile = null;  // 현재 마우스가 가리키고 있는 타일
 
     private void Awake()
     {
@@ -20,7 +23,7 @@ public class DraggableUnit : MonoBehaviour
 
     private void OnMouseDown()
     {
-        originY = transform.position.y;
+        originPos = transform.position;
 
         dragPlane = new Plane(Vector3.up, Vector3.zero);
         isDragging = true;
@@ -31,18 +34,37 @@ public class DraggableUnit : MonoBehaviour
         if (!isDragging) return;
 
         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        // 유닛 드래그
         if (dragPlane.Raycast(ray, out float enter))
         {
             Vector3 hitPoint = ray.GetPoint(enter);
             transform.position = new Vector3(hitPoint.x, hitPoint.y + offsetY, hitPoint.z);
         }
+
+        // 타일 감지
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, tileLayerMask))
+            hoveredTile = hit.collider.gameObject;
+        else
+            hoveredTile = null;
     }
 
     private void OnMouseUp()
     {
         isDragging = false;
 
-        Vector3 pos = transform.position;
-        transform.position = new Vector3(pos.x, originY, pos.z);
+        if (hoveredTile != null)
+        {
+            Debug.Log("타일 발견");
+            Vector3 targetPos = hoveredTile.transform.position;
+            transform.position = new Vector3(targetPos.x, originPos.y, targetPos.z);
+            hoveredTile = null;
+        }
+        else
+        {
+            Debug.Log("타일 발견 X");
+            transform.position = originPos;
+            hoveredTile = null;
+        }
     }
 }
