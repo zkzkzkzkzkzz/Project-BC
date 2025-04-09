d50cfe47a6ed38a051504aaae25cae189984e3cb
zkzkzkzkzkzz <sto134679@gmail.com>
2025-04-09 14:33:08 +0900

    FEAT / 타일 클래스 추가

---
+++ Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType { Board, Bench }
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private TileType tileType;                 // 타일 타입
+    [SerializeField] private Unit occupyingUnit = null;   // 타일 점유중인 유닛
+
+    [SerializeField] private Vector3 coord;     // 보드용 좌표
+    [SerializeField] private int index;         // 벤치용 인덱스
+
+
+    public TileType GetTileType()
+    {
+        return tileType;
+    }
+    public void SetTileType(TileType _tileType)
+    {
+        tileType = _tileType;
+    }
+
+    public Unit GetOccupyingUnit()
+    {
+        return occupyingUnit;
+    }
+    public void SetOccupyingUnit(Unit _occupyingUnit)
+    {
+        occupyingUnit = _occupyingUnit;
+    }
+
+
+    public Vector3 BoardCoord
+    {
+        get
+        {
+            if (tileType != TileType.Board)
+                Debug.LogError("타일의 타입이 Board가 아닙니다");
+            return coord;
+        }
+        set
+        {
+            coord = value;
+        }
+    }
+
+    public int BenchIndex
+    {
+        get
+        {
+            if (tileType != TileType.Bench)
+                Debug.LogError("타일의 타입이 Bench가 아닙니다");
+            return index;
+        }
+        set
+        {
+            index = value;
+        }
+    }
+}
