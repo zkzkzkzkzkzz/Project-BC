b3a767b548447e63ca857ee756cf2750e0eda7aa
zkzkzkzkzkzz <sto134679@gmail.com>
2025-04-14 14:32:11 +0900

    FEAT / 더미용 적들이 랜덤한 위치에 스폰되도록 변경

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
@@ -19,11 +20,37 @@ public class BattleManager : MonoBehaviour
 
 
 
-    /// <summary>
-    /// 
-    /// </summary>
     public void BattleStart()
     {
-        Debug.Log("전투 시작");
+
+    }
+
+
+
+    // @@@@ 디버그용 더미 적 생성
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float unitYoffset = 1f;
+    public void SpawnDummyEnemy(int count)
+    {
+        var tiles = BoardManager.Instance.GetBoardTiles();
+
+        List<Tile> tileList = tiles.Where(t => !t.IsOccupied() && t.BoardCoord.z > 3f).ToList();
+
+        for (int i = 0; i < count; ++i)
+        { 
+            int randIdx = Random.Range(0, tileList.Count);
+            Tile tile = tileList[randIdx];
+            tileList.RemoveAt(randIdx);
+         
+            Vector3 spawnPos = tile.transform.position + Vector3.up * unitYoffset;
+            GameObject enemyObj = Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            Unit enemy = enemyObj.GetComponent<Unit>();
+
+            enemy.SetOwnerId(1);
+            enemy.SetCurUnitTile(tile);
+            tile.SetOccupyingUnit(enemy);
+
+            enemyObj.name = $"DummyEnemy_{i}";
+        }
     }
 }
